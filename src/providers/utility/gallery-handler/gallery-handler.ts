8238d9108c5e2d4c56ba0fd485f7efe510e1de0d
rambo14122 <31562307+rambo14122@users.noreply.github.com>
2018-01-03 11:21:02 +0800

    update

--- src/providers/utility/gallery-handler/gallery-handler.ts
+++ src/providers/utility/gallery-handler/gallery-handler.ts
@@ -29,8 +29,7 @@ export class GalleryHandlerProvider {
         });
       });
     }
-    else
-    {
+    else {
       this.getImageFromGalleryFurther(numberOfImage);
     }
 
@@ -40,41 +39,36 @@ export class GalleryHandlerProvider {
     let galleryOptions =
       {
         maximumImagesCount: numberOfImage,
-        width: 800
+        width: 500,
+        quality: 50,
+        outputType: 1
       };
     var promise = new Promise((resolve, reject) => {
       this.imagePicker.getPictures(galleryOptions).then((results) => {
-          for (var i = 0; i < results.length; i++) {
-            console.log('Image URI: ' + results[i]);
-
-          }
           if (numberOfImage == 1) {
-            var url = results[0];
-            (<any>window).FilePath.resolveNativePath(url, (result) => {
-              var nativePath = result;
-              (<any>window).resolveLocalFileSystemURL(nativePath, (res) => {
-                res.file((resFile) => {
-                  var reader = new FileReader();
-                  reader.readAsArrayBuffer(resFile);
-                  reader.onloadend = (evt: any) => {
-                    var imgBlob = new Blob([evt.target.result], {type: 'image/jpeg'});
-                    var imageStore = this.fireStore.ref('/userProfileImages').child(firebase.auth().currentUser.uid);
-                    imageStore.put(imgBlob).then((res) => {
-                      this.fireStore.ref('/userProfileImages').child(firebase.auth().currentUser.uid).getDownloadURL().then((url) => {
-                        resolve(url);
-                      }).catch((err) => {
-                        reject(err);
-                      })
-                    }).catch((err) => {
-                      reject(err);
-                    })
-                  }
-                })
+            var base64String = results[0];
+            var byteCharacters = atob(base64String);
+            var byteNumbers = new Array(byteCharacters.length);
+            for (var i = 0; i < byteCharacters.length; i++) {
+              byteNumbers[i] = byteCharacters.charCodeAt(i);
+            }
+            var byteArray = new Uint8Array(byteNumbers);
+            var imgBlob = new Blob([byteArray], {type: 'image/jpeg'});
+            var imageStore = this.fireStore.ref('/userProfileImages').child(firebase.auth().currentUser.uid);
+            imageStore.put(imgBlob).then((res) => {
+              this.fireStore.ref('/userProfileImages').child(firebase.auth().currentUser.uid).getDownloadURL().then((url) => {
+                resolve(url);
+              }).catch((err) => {
+                reject(err);
               })
-            })
+            }).catch((err) => {
+              reject(err);
+            });
           }
         }
-      );
+      ).catch((err) => {
+        console.log(err);
+      });
     });
   }
 
